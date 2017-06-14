b8ce965b1c7d2c32eab9243f83ba39296fd2f699
Alex <shlemko.alexey@gmail.com>
2017-06-15 01:45:13 +0300

    1

---
+++ js/objects.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+  $('.objects-block-carouser').slick({
+    accessibility: true,
+    arrows: false,
+    dots: false,
+    speed: 500,
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 1
+  });
+
+  $('.objects-block-units').slick({
+    accessibility: true,
+    arrows: false,
+    dots: false,
+    speed: 500,
+    infinite: true,
+    fade: true
+  });
+
+
+});
+
+
+$(".arrow-left").on("click", function(){
+  $('.objects-block-carouser').slick("slickPrev");
+});
+
+$(".arrow-right").on("click", function(){
+  $('.objects-block-carouser').slick("slickNext");
+});
+$(".objects-block-carouser img").on("click", function(){
+  $(".objects-block-carouser img").removeClass("active");
+  $(this).addClass("active");
+})
+
+
+$(".objects-block-carouser img:nth-child(1)").on("click", function(){
+  $('.objects-block-units').slick("slickGoTo", 0);
+});
+$(".objects-block-carouser img:nth-child(2)").on("click", function(){
+  $('.objects-block-units').slick("slickGoTo", 1);
+});
+$(".objects-block-carouser img:nth-child(3)").on("click", function(){
+  $('.objects-block-units').slick("slickGoTo", 2);
+});
+$(".objects-block-carouser img:nth-child(4)").on("click", function(){
+  $('.objects-block-units').slick("slickGoTo", 3);
+});
+$(".objects-block-carouser img:nth-child(5)").on("click", function(){
+  $('.objects-block-units').slick("slickGoTo", 4);
+});
+$(".objects-block-carouser img:nth-child(6)").on("click", function(){
+  $('.objects-block-units').slick("slickGoTo", 5);
+});
+$(".objects-block-carouser img:nth-child(7)").on("click", function(){
+  $('.objects-block-units').slick("slickGoTo", 1);
+});
+$(".objects-block-carouser img:nth-child(8)").on("click", function(){
+  $('.objects-block-units').slick("slickGoTo", 2);
+});
+$(".objects-block-carouser img:nth-child(9)").on("click", function(){
+  $('.objects-block-units').slick("slickGoTo", 3);
+});
